6e6c81fbde37a648d47ae7d2d255d166293375e6
nahuel dintal <nahue.dintal@gmail.com>
2025-10-09 13:30:09 -0300

    modificacion de la consola

--- Data/obj/Debug/net9.0/Data.AssemblyInfo.cs
+++ Data/obj/Debug/net9.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18bbdc3cbb10b4bc7e79506d07533e2ba7fbda00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51f0bebb7c89d0310355c44d4910cd1919b5269b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
